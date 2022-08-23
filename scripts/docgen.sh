5d387c9142b5502023aee6938fb14eac58d92f09
Brian Drelling <brian@drelling.com>
2022-08-23 11:04:52 -0500

    Regenerated docs, updated examples

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -14,19 +14,20 @@ target='DocCMiddleware'
 # If this is left empty, the default location is used.
 output_directory=${1-'Docs'}
 
+# Recreate the output directory if it already exists.
+rm -rf $output_directory
+mkdir $output_directory
+
 # Define a function for generating a .doccarchive.
 generate_archive() {
     target=$1
 
-    echo "Generating archive for ${product} in ${output_directory}..."
+    echo "Generating archive for ${target} in ${output_directory}..."
 
     # If we have defined an output directory, use it.
     # Otherwise, generate documentation in the default location.
     # At time of writing, this is: .build/plugins/Swift-DocC/outputs/
     if [ ! -z $output_directory ]; then
-        # Create the output directory if it does not already exist.
-        mkdir $output_directory
-
         swift package --allow-writing-to-directory $output_directory \
             generate-documentation \
             --hosting-base-path $target \
@@ -37,12 +38,6 @@ generate_archive() {
             --hosting-base-path $target \
             --target $target
     fi
-
-    # To add a hosting base path, add the following option:
-    # --hosting-base-path $target
-
-    # TODO: Once we can confirm the above is working, try enabling the following flags:
-    # --disable-indexing # apparently only necessary for IDE documentation navigation
 }
 
 # Add new entries for every target here.
